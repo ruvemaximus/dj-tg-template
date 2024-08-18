0beeac7e6799909117d5511319c64949ea4c3df3
Никита <ruvemaximus@gmail.com>
2024-08-18 19:47:14 +0800

    improve(bot): авторегистрация подключенных приложений + логгер

--- accounts/router.py
+++ accounts/router.py
@@ -1,4 +1,12 @@
-from . import views
-from telegram.ext import CommandHandler
+import logging
+from telegram import Update
+from telegram.ext import CommandHandler, ContextTypes
 
-HANDLERS = [CommandHandler("start", views.cmd_start)]
+logger = logging.getLogger(__name__)
+
+
+async def cmd_start(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
+    await update.message.reply_text("Hi!")
+
+
+HANDLERS = [CommandHandler("start", cmd_start)]
--- bot.py
+++ bot.py
@@ -2,34 +2,34 @@
 
 import logging
 import os
+from importlib import import_module
 
 import django
 from dotenv import load_dotenv
 from telegram.ext import Application
 
+from core.logger import init_logger
+
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "tg_bot.settings")
 django.setup()
-
-# NOTE: мы не можем использовать модули до `django.setup()`
-import accounts.router # noqa
 
 logging.getLogger("httpx").setLevel(logging.WARNING)
 
 
 def main() -> None:
     load_dotenv()
+    init_logger("bot")
 
     application = Application.builder().token(os.getenv("TG_BOT_TOKEN")).build()
 
-    application.add_handlers(accounts.router.HANDLERS)
+    for app_name in django.conf.settings.INSTALLED_APPS:
+        if app_name.startswith("django."):
+            continue
+
+        application.add_handlers(import_module(f"{app_name}.router").HANDLERS)
 
     application.run_polling()
 
 
 if __name__ == "__main__":
-    logging.basicConfig(
-        format="%(asctime)s [%(name)s] %(levelname)s - %(message)s",
-        level=logging.INFO,
-        datefmt="%Y-%m-%d %H:%M:%S",
-    )
     main()
