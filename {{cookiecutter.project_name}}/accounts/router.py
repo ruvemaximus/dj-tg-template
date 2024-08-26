1348db016795f2610083dec2548eebe8ccc822dd
Никита <ruvemaximus@gmail.com>
2024-08-27 00:13:57 +0800

    feat: добавлена команда для задания команд

--- {{cookiecutter.project_name}}/accounts/router.py
+++ {{cookiecutter.project_name}}/accounts/router.py
@@ -10,4 +10,10 @@ async def cmd_start(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
     await update.message.reply_text("Hi!")
 
 
-HANDLERS = [CommandHandler("start", cmd_start)]
+async def cmd_set_commands(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
+    bot = update.get_bot()
+    await bot.delete_my_commands()
+    await bot.set_my_commands([("start", "Начать диалог")])
+
+
+HANDLERS = [CommandHandler("start", cmd_start), CommandHandler("set_commands", cmd_set_commands)]
